c2b668d543b0b4ee02421ff3adf1b94b4e273944
Moonwalkert3ch <6620@holbertonstudents.com>
2024-10-21 16:27:27 -0500

    created logout function to display when user is logged in

--- atlas-holbegram/app/login.tsx
+++ atlas-holbegram/app/login.tsx
@@ -9,7 +9,7 @@ export default function Page() {
                 <Text>Create a new account!</Text>
             </Link>
             <Pressable onPress={() => {
-                router.push("/(tabs)/");
+                router.replace("/(tabs)/");
             }}>
                 <Text>Sign In!</Text>
             </Pressable>
